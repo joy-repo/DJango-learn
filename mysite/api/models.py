1e3924d650cd21c539124eabd192da71ceb12614
Somrrita <java.indranil@gmail.com>
2025-04-23 18:19:45 +0530

    commit

--- mysite/api/models.py
+++ mysite/api/models.py
@@ -6,7 +6,7 @@ class BlogPost(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
     published_date = models.DateTimeField(auto_now_add=True)
-    
+    id = models.AutoField(primary_key=True)
 
     def __str__(self):
         return self.title
--- mysite/api/views.py
+++ mysite/api/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
 from rest_framework import generics
+from rest_framework.response import Response, status
 from .models import BlogPost
 from .serializers import BlogPostSerializer
+from rest_framework.views import APIView
 
 
 # Create your views here.
@@ -9,3 +11,23 @@ from .serializers import BlogPostSerializer
 class BlogPostList(generics.ListCreateAPIView):
     queryset = BlogPost.objects.all()
     serializer_class = BlogPostSerializer
+
+    def delete(self, request, *args, **kwargs):
+        BlogPost.objects.all().delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+        
+
+class BlogPostRetrieveUpdateDestroyAPIView(generics.RetrieveUpdateDestroyAPIView):
+    queryset = BlogPost.objects.all()
+    serializer_class = BlogPostSerializer
+    lookup_field = 'pk'  # Use 'id' as the lookup field instead of the default 'pk'
+
+class BlogPostDetail(APIView):
+
+    def get(self, request, pk):
+        try:
+            blog_post = BlogPost.objects.get(pk=pk)
+            serializer = BlogPostSerializer(blog_post)
+            return Response(serializer.data, status=status.HTTP_200_OK)
+        except BlogPost.DoesNotExist:
+            return Response(status=status.HTTP_404_NOT_FOUND)
